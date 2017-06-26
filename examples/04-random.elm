162dd4a6e47f9805841b512e07ef8206f1cd9ca0
Alexandre Wavelet <alexandre.wavelet@careerbuilder.com>
2017-06-26 10:14:20 +0200

    Add die face images

--- examples/04-random.elm
+++ examples/04-random.elm
@@ -1,4 +1,5 @@
 import Html exposing (..)
+import Html.Attributes exposing (src, alt)
 import Html.Events exposing (..)
 import Random
 
@@ -62,6 +63,28 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ h1 [] [ text (toString model.dieFace) ]
+    [ div [] [ dieFaceImage model.dieFace ]
     , button [ onClick Roll ] [ text "Roll" ]
     ]
+
+dieFaceImage : number -> Html Msg
+dieFaceImage dieFace =
+  let
+    (fileName) =
+      case dieFace of
+        1 ->
+          "1.png"
+        2 ->
+          "2.png"
+        3 ->
+          "3.png"
+        4 ->
+          "4.png"
+        5 ->
+          "5.png"
+        6 ->
+          "6.png"
+        _ ->
+          Debug.crash "This shouldn't happen"
+  in
+    img [ src ("images/" ++ fileName), alt (toString dieFace ++ " face")] []
