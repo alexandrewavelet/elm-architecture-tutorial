56cb58946910d73395bbbcfdb02a038df6853b86
Alexandre Wavelet <alexandre.wavelet@careerbuilder.com>
2017-06-26 11:00:00 +0200

    Added second dice

--- examples/04-random.elm
+++ examples/04-random.elm
@@ -19,13 +19,14 @@ main =
 
 
 type alias Model =
-  { dieFace : Int
+  { dieFace1 : Int
+  , dieFace2 : Int
   }
 
 
 init : (Model, Cmd Msg)
 init =
-  (Model 1, Cmd.none)
+  (Model 1 1, Cmd.none)
 
 
 
@@ -34,19 +35,21 @@ init =
 
 type Msg
   = Roll
-  | NewFace Int
+  | NewFace1 Int
+  | NewFace2 Int
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Roll ->
-      (model, Random.generate NewFace (Random.int 1 6))
+      (model, Random.generate NewFace1 (Random.int 1 6))
 
-    NewFace newFace ->
-      (Model newFace, Cmd.none)
+    NewFace1 newFace ->
+      ({model | dieFace1 = newFace}, Random.generate NewFace2 (Random.int 1 6))
 
-
+    NewFace2 newFace ->
+      ({model | dieFace2 = newFace}, Cmd.none)
 
 -- SUBSCRIPTIONS
 
@@ -63,7 +66,8 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ div [] [ dieFaceImage model.dieFace ]
+    [ div [] [ dieFaceImage model.dieFace1 ]
+    , div [] [ dieFaceImage model.dieFace2 ]
     , button [ onClick Roll ] [ text "Roll" ]
     ]
 
