f12a618c93e68a0d90dcb06f68a4df090685ceb7
Alexandre Wavelet <alexandre.wavelet@careerbuilder.com>
2017-06-28 14:39:27 +0200

    Mark tasks as complete

--- examples/10-tasklist.elm
+++ examples/10-tasklist.elm
@@ -2,7 +2,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import String.Extra exposing (unquote)
-
+import List.Extra exposing (updateIf)
 
 main =
   Html.beginnerProgram
@@ -18,15 +18,16 @@ main =
 type alias Model =
   { input : String
   , taskList : List Task
+  , currentId : Int
   }
 
-type alias Task = { task : String, complete : Bool }
+type alias Task = { id : Int, task : String, complete : Bool }
 
 type Visibility = All | Active | Completed
 
 model : Model
 model =
-  Model "" []
+  Model "" [] 0
 
 -- UPDATE
 
@@ -34,17 +35,43 @@ model =
 type Msg
   = InputTask String
   | Add
+  | MarkComplete Int String
 
 
 update : Msg -> Model -> Model
-update msg {input, taskList} =
+update msg {input, taskList, currentId} =
   case msg of
     InputTask newInput ->
-      Model (unquote <| toString newInput) taskList
+      -- Unquote - BCUZ IT WORKS - PROBLEM!?
+      Model (toString newInput |> unquote) taskList currentId
 
     Add ->
-      Model "" ((Task input False) :: taskList)
+      Model
+        ""
+        ((newTask currentId input)
+          |> addTask taskList
+        )
+        (increment currentId)
 
+    MarkComplete id task->
+      -- Find Task and update
+      let
+        updatedTask =
+          Task id task True
+      in
+        Model input (updateIf (\task -> task.id == id) (\task -> { task | complete = True }) taskList) currentId
+
+increment : Int -> Int
+increment int =
+  int + 1
+
+newTask : Int -> String -> Task
+newTask id name =
+  Task id name False
+
+addTask : List Task -> Task -> List Task
+addTask list task =
+  task :: list
 
 -- VIEW
 
@@ -67,5 +94,14 @@ displayTask task =
       else
         "red"
   in
-  li [style [("color", color)]] [text task.task]
+  li [style [("color", color)]]
+    [text ((toString task.task |> unquote) ++ " - ")
+    , displayCompletionButton task
+    ]
 
+displayCompletionButton : Task -> Html Msg
+displayCompletionButton task =
+  if not task.complete then
+    a [onClick (MarkComplete task.id task.task)] [text "mark as complete"]
+  else
+    text ""
